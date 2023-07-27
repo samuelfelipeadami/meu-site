9aee8ef384a1ee3df2d91ad7b3e8566ffce52e95
samuelfelipeadami <140742078+samuelfelipeadami@users.noreply.github.com>
2023-07-27 14:22:23 -0300

    Add files via upload

---
+++ parabens.js
@@ -0,0 +1,9 @@
+// Adicione um evento de clique ao botão "Sim" para redirecionar para a URL desejada
+document.getElementById('btSim').addEventListener('click', function() {
+    window.location.href = "soldiers.html";
+});
+
+// Adicione um evento de clique ao botão "Não" para redirecionar para a página do calendário
+document.getElementById('btnNao').addEventListener('click', function() {
+    window.location.href = "index.html";
+});
